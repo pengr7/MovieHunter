f8af77347de7c8b67a40e4280fd4baacb0f9d7e0
xujunxijc <360499805@qq.com>
2017-06-27 23:10:11 +0800

    Update rubik.h

--- src/rubik.h
+++ src/rubik.h
@@ -10,7 +10,7 @@ private:
 	GLfloat color[6][3]; //六个面的颜色
 	cube* Cube[3][3][3]; //魔方左后下角块为(0,0,0)，向左X轴正，向前Z轴正，向上Y轴正。
 public:
-	rubik(GLfloat* p, GLfloat* d, GLfloat* c[], GLfloat l); //需要初始化Cube数组
+	rubik(GLfloat p[], GLfloat d[], GLfloat c[][3], GLfloat l); //需要初始化Cube数组
 	~rubik();
 	GLfloat * getPosition();
 	void setPosition(GLfloat* p);
@@ -24,9 +24,9 @@ public:
 	void setLength(GLfloat l);
 
 	cube* getACube(int x, int y, int z);  //返回Cube[x][y][z]的指针
-	void AllCubeDO(void (*a)(void)); //对所有Cube使用函数a
-	//void RotatRubik(int xyz, int dir, GLfloat angle, int update);  
-	//xyz = 0/1/2时对应围绕X/Y/Z轴转angle度，dir = 0/1为顺/逆时针,update为0不需更新Cube数组,1为需要更新。
+
+	void AllCubeDO(void (*fuc)(void)); //对所有Cube使用函数fuc
+
 	void RotatPlane(int xyz, int num, int dir, GLfloat angle, int update); 
 	//xyz = 0， num = 1 即为旋转 x = 1 的平面 xyz = 1, num = 2即为旋转 y = 2的面，类推，num = 3为同时旋转3个面，即整个魔方
 	//dir = 0/1为顺/逆时针, angle为旋转角度，update为0不需更新Cube数组,1为需要更新。
